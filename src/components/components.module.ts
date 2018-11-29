95d2479419f5daa25cbba8821c18e914b734b61b
Bayan-Abuawad <bayan.c.r.1@gmail.com>
2018-11-29 16:36:31 +0200

    auto complease component logic inhancement

--- src/components/components.module.ts
+++ src/components/components.module.ts
@@ -1,8 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RwIonic3GoogleMapsAutocompleteInputComponent } from './rw-ionic3-google-maps-autocomplete-input/rw-ionic3-google-maps-autocomplete-input';
+import { IonicModule } from 'ionic-angular';
 @NgModule({
 	declarations: [RwIonic3GoogleMapsAutocompleteInputComponent],
-	imports: [],
+	imports: [
+		IonicModule
+	],
 	exports: [RwIonic3GoogleMapsAutocompleteInputComponent]
 })
 export class RWIonic3GoogleMapsAutoCompleteComponentsModule { }
--- src/components/rw-ionic3-google-maps-autocomplete-input/rw-ionic3-google-maps-autocomplete-input.ts
+++ src/components/rw-ionic3-google-maps-autocomplete-input/rw-ionic3-google-maps-autocomplete-input.ts
@@ -1,15 +1,116 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, NgZone, Input, ViewChild, ElementRef } from '@angular/core';
+import { ControlValueAccessor, NG_VALUE_ACCESSOR, Validator, NG_VALIDATORS, NgForm } from '@angular/forms';
+
+declare const google: any;
 
 @Component({
   selector: 'rw-ionic3-google-maps-autocomplete-input',
-  templateUrl: 'rw-ionic3-google-maps-autocomplete-input.html'
+  templateUrl: 'rw-ionic3-google-maps-autocomplete-input.html',
+  providers: [
+    { provide: NG_VALUE_ACCESSOR, useExisting: RwIonic3GoogleMapsAutocompleteInputComponent, multi: true },
+    { provide: NG_VALIDATORS, useExisting: RwIonic3GoogleMapsAutocompleteInputComponent, multi: true }
+  ]
+
 })
-export class RwIonic3GoogleMapsAutocompleteInputComponent {
+export class RwIonic3GoogleMapsAutocompleteInputComponent implements ControlValueAccessor, OnInit, Validator {
 
-  text: string;
+  @Input() required: boolean = false;
+  @Input() placeholder: string = "";
+  @Input() form: NgForm;
 
-  constructor() {
-    this.text = 'Hello World';
+  @ViewChild('inputContainer') inputContainer: ElementRef;
+
+  GoogleAutocomplete: any;
+  autocomplete: IFormGooglePlacesInput;
+  autocompleteItems: any[];
+  geocoder: any;
+  onchange: any;
+  inputWasFocused: boolean = false;
+  constructor(public zone: NgZone) {
+    this.GoogleAutocomplete = new google.maps.places.AutocompleteService();
+    this.geocoder = new google.maps.Geocoder;
+    this.autocomplete = { value: '', position: { lat: 0, lng: 0 } };
+    this.autocompleteItems = [];
+  }
+  ngOnInit(): void { }
+  isFormSubmitted() {
+    if (this.form)
+      return this.form.submitted;
+    return false;
+  }
+  onBlur() {
+    this.inputWasFocused = true;
+    this.validate();
+  }
+  updateSearchResults() {
+    const query = this.autocomplete.value.trim();
+    if (query == '') {
+      this.autocompleteItems = [];
+      return;
+    } else if (query.length > 2) {
+      this.GoogleAutocomplete.getPlacePredictions({ input: query },
+        (predictions, status) => {
+          this.autocompleteItems = [];
+          this.zone.run(() => {
+            if (predictions) {
+              predictions.forEach((prediction) => {
+                if (this.autocompleteItems.length < 3)
+                  this.autocompleteItems.push(prediction);
+              });
+            }
+          });
+        });
+    }
   }
 
+  selectSearchResult(item) {
+    console.log(item);
+
+    this.autocomplete.value = item.description
+    this.autocompleteItems = [];
+    this.geocoder.geocode({ 'placeId': item.place_id }, (results, status) => {
+      if (status === 'OK' && results[0]) {
+        this.autocomplete.position = {
+          lat: results[0].geometry.location.lat(),
+          lng: results[0].geometry.location.lng()
+        };
+
+        this.onchange(this.autocomplete);
+
+      }
+    })
+  }
+
+  registerOnChange(fn: any): void {
+    this.onchange = fn;
+  }
+  validate() {
+    //we need to find a way to change text color on error    
+    console.log(this.inputContainer);
+
+    if ((!this.autocomplete.value || !this.autocomplete.position.lat || !this.autocomplete.position.lng) && this.required && this.inputWasFocused) {
+      // this.inputContainer.nativeElement.className = 'input-container input-container-error';
+      return {
+        "location": "location not valid"
+      };
+    }
+    // this.inputContainer.nativeElement.className = 'input-container';
+    return null
+  }
+  writeValue(value: IFormGooglePlacesInput): void {
+    if (value) {
+      this.autocomplete = value;
+    }
+  }
+  registerOnTouched(fn: any): void { }
+  setDisabledState?(isDisabled: boolean): void { }
+  registerOnValidatorChange?(fn: () => void): void { }
+}
+
+export interface IFormGooglePlacesInput {
+  value: string;
+  position: {
+    lat: string | number;
+    lng: string | number
+  }
 }
